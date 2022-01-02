d3ed30420269b2dcf7f970df804522a83089168c
luckyfie <81020824+luckyfie@users.noreply.github.com>
2022-01-02 17:02:53 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
+
+['command_handler', 'event_handler'].forEach(handler => {
+    require(`./handlers/${handler}`)(client, Discord);
+})
+
+
+
+
+
+
+
+client.login(process.env.token);
+
+// process.env.token
+// dO U SSE,
+// ODYxMTc1NDAyNTcwNTc5OTY5.YOF-Fw.hlvZKY59e5Bp6OdrMb_pQrRncFY
+
+
